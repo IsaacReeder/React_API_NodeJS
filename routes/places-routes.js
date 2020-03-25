6e1263501f05552ef97e2af03ff844cf56b69ca5
Isaac R <isaacreeder5@gmail.com>
2020-03-25 12:53:55 -0700

    finished MVC

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,51 +1,39 @@
-const express = require("express");
+const express = require('express');
+const { check } = require('express-validator');
+
+const placesControllers = require('../controllers/places-controllers');
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1"
-  }
-];
+router.get('/:pid', placesControllers.getPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid; // { pid: 'p1' }
+router.get('/user/:uid', placesControllers.getPlacesByUserId);
 
-  const place = DUMMY_PLACES.find(p => {
-    return p.id === placeId;
-  });
+router.post(
+  '/',
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+    check('description').isLength({ min: 5 }),
+    check('address')
+      .not()
+      .isEmpty()
+  ],
+  placesControllers.createPlace
+);
 
-  if (!place) {
-    const error = new Error("Could not find a place for the provided id.");
-    error.code = 404;
-    throw error;
-  }
+router.patch(
+  '/:pid',
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+    check('description').isLength({ min: 5 })
+  ],
+  placesControllers.updatePlace
+);
 
-  res.json({ place }); // => { place } => { place: place }
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-
-  const place = DUMMY_PLACES.find(p => {
-    return p.creator === userId;
-  });
-
-  if (!place) {
-    const error = new Error("Could not find a place for the provided user id.");
-    error.code = 404;
-    return next(error);
-  }
-
-  res.json({ place });
-});
+router.delete('/:pid', placesControllers.deletePlace);
 
 module.exports = router;
